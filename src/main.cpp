b6f27a85585c864fe9428daf0a5fe8bbb70b4884
jmoorehead <i7611989@bournemouth.ac.uk>
2015-10-13 11:58:57 +0100

    basic class working

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <cstdlib>
 
+#include "ImageWrite.h"
 int main()
 {
   std::cout<<"working\n";
+  ImageWrite myImage(200,200);
+  myImage.setPixel(400,50,255,255,255);
   return EXIT_SUCCESS;
 }
